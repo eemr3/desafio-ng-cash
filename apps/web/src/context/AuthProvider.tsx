1c1fd8a97bd3c816678a3292fc2c38a6722650c0
eemr3 <eemr3@yahoo.com.br>
2022-11-21 20:02:30 -0300

    feat: :zap: AuthProvider.tsx
    create auth provider

---
+++ apps/web/src/context/AuthProvider.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode, useState } from 'react';
+import { api } from '../server/http';
+import Cookies from 'js-cookie';
+
+interface IContext {
+  signIn: (data: IUser) => Promise<void>;
+  user: string | null;
+  isAuthetication: boolean;
+  signOut: () => void;
+}
+
+interface IUser {
+  username: string;
+  password: string;
+}
+
+export const AuthContext = createContext({} as IContext);
+
+export default function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<any | null>(null);
+  const isAuthetication = !!user;
+  const signIn = async ({ username, password }: IUser) => {
+    const response = await api.post('/login', {
+      username,
+      password,
+    });
+    Cookies.set('authToken', response.data.token);
+    setUser(response.data);
+  };
+
+  const signOut = () => {
+    Cookies.remove('authToken');
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ signIn, user, isAuthetication, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
