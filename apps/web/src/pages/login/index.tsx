697d4f85a54a00230ca583c6a9621463d1c5add4
eemr3 <eemr3@yahoo.com.br>
2022-11-21 17:13:10 -0300

    refactor: :memo: login/indextsx
    replace state with formik

--- apps/web/src/pages/login/index.tsx
+++ apps/web/src/pages/login/index.tsx
@@ -1,37 +1,21 @@
 import { LockClosedIcon } from '@heroicons/react/20/solid';
 import Link from 'next/link';
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { useFormik } from 'formik';
 import { CustomInput } from '../../components/CustomInput';
 import { CustomPasswordInput } from '../../components/CustomPasswordInput';
-
-const checkName = (name: string) => /^[a-zA-ZÀ-ü ]{3}/g.test(name);
-const checkPassword = (password: string) =>
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/g.test(password);
+import { schema } from './schema';
 
 export default function Login() {
-  const [inputValues, setInputValues] = useState({
-    username: '',
-    password: '',
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: schema,
+    onSubmit: () => {
+      console.log(formik.values);
+    },
   });
-  const [error, setError] = useState('');
-
-  const handleInputValues = (event: ChangeEvent<HTMLInputElement>) => {
-    const name = event.target.name;
-    setInputValues({ ...inputValues, [name]: event.target.value });
-  };
-
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (!checkName(inputValues.username)) {
-      return setError('Nome de usário incorreto');
-    }
-    if (!checkPassword(inputValues.password)) {
-      setError('');
-      return setError('Password formato incorreto');
-    }
-    console.log(inputValues);
-  };
-
   return (
     <>
       <div
@@ -48,7 +32,7 @@ export default function Login() {
             </h2>
           </div>
           <form
-            onSubmit={handleSubmit}
+            onSubmit={formik.handleSubmit}
             className="mt-8 space-y-6"
             action="#"
             method="POST"
@@ -61,27 +45,27 @@ export default function Login() {
                 type="text"
                 autoComplete="username"
                 required
-                value={inputValues.username}
-                onChange={handleInputValues}
+                value={formik.values.username}
+                onChange={formik.handleChange}
                 className="relative block w-full appearance-none rounded-none 
                   rounded-t-md border border-gray-300 px-3 py-2 text-gray-900
                   placeholder-gray-500 focus:z-10 focus:border-indigo-500 
                   focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 placeholder="Nome de usuário"
               />
-              {error ? <p>{error}</p> : ''}
+              {formik.errors.username && <p>{formik.errors.username}</p>}
               <CustomPasswordInput
                 label="Senha"
                 id="password"
                 name="password"
-                value={inputValues.password}
-                onChange={handleInputValues}
+                value={formik.values.password}
+                onChange={formik.handleChange}
                 className="relative block w-full appearance-none rounded-none 
                 rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 
                 placeholder-gray-500 focus:z-10 focus:border-indigo-500 
                 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               />
-              {error ? <p>{error}</p> : ''}
+              {formik.errors.password && <p>{formik.errors.password}</p>}
             </div>
 
             <div className="mt-4">
