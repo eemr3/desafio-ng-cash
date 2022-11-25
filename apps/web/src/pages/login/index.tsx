65521e1017a0d2e2f986498486fcc1925c7d454f
eemr3 <eemr3@yahoo.com.br>
2022-11-25 18:06:45 -0300

    style: :art: ajustes no estilo dos botões login e registro

--- apps/web/src/pages/login/index.tsx
+++ apps/web/src/pages/login/index.tsx
@@ -89,13 +89,13 @@ export default function Login() {
               <button
                 type="submit"
                 className="group relative flex w-full justify-center rounded-md 
-                  border border-transparent bg-indigo-600 py-2 px-4 text-sm 
-                  font-medium text-white hover:bg-indigo-700 focus:outline-none 
-                  focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50"
+                  border border-transparent bg-[#49aa26] py-2 px-4 text-sm 
+                  font-medium text-white hover:bg-[#37801d] focus:outline-none 
+                  focus:ring-2 focus:ring-[#52bf2b] focus:ring-offset-2 disabled:opacity-50"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <LockClosedIcon
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    className="h-5 w-5 text-[#58bb34] group-hover:text-[#5fdc32]"
                     aria-hidden="true"
                   />
                 </span>
--- apps/web/src/pages/register/index.tsx
+++ apps/web/src/pages/register/index.tsx
@@ -99,10 +99,10 @@ export default function Register() {
             <div>
               <button
                 type="submit"
-                className="group relative flex w-full justify-center rounded-md border 
-                    border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium 
-                    text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 
-                    focus:ring-indigo-500 focus:ring-offset-2"
+                className="group relative flex w-full justify-center rounded-md 
+                  border border-transparent bg-[#49aa26] py-2 px-4 text-sm 
+                  font-medium text-white hover:bg-[#37801d] focus:outline-none 
+                  focus:ring-2 focus:ring-[#52bf2b] focus:ring-offset-2 disabled:opacity-50"
               >
                 Salvar
               </button>
